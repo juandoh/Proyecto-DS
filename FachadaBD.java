37f1dc2e9010607ff85adaea0fda9cc87a9c2297
juandoh <juan.ordonez.huila@correounivalle.edu.co>
2017-10-25 02:31:08 -0500

    Add files via upload

---
+++ FachadaBD.java
@@ -0,0 +1,73 @@
+package accesoDatos;
+
+/**
+ * 
+ * @author juan diego ordoñez
+ * @version 20 octubre
+ *
+ */
+
+import java.sql.*;
+
+public class FachadaBD {
+	
+	String url, usuario, password;
+	Connection conexion = null;
+	Statement instrucion;
+	ResultSet tabla;
+
+	public FachadaBD() {
+		// TODO Auto-generated constructor stub
+	url = "jdbc:postgresql://localhost:5432/talleres";
+	usuario = "juandiego";
+	password = "juancrash";
+	
+	}
+	
+	public Connection conectar(){
+		try {
+			//Se carga el drive
+			Class.forName("org.postgresql.Driver");
+			
+		} catch ( Exception e) {
+			System.out.println( "No se pudo cargar el driver" );
+		}
+		
+		try {
+			//Crear el objeto de de conexion a la base de datos
+			conexion = DriverManager.getConnection(url, usuario, password);
+			System.out.println( "Conexion Abierta" );
+			
+			return conexion;
+		} catch ( Exception e ) {
+			System.out.println( "No se pudo abrir la base de datos" );
+			return null;
+		}
+	}//Fin del metodo conectar
+	
+	public Connection getConnetion(){
+		if (conexion == null){
+			return this.conectar();
+		} 
+		else{
+			return conexion;
+		}
+	}
+	
+	public void closeConection (Connection c){
+		try{
+			if (conexion!=null){
+				c.close();
+			} 
+		} catch (Exception e){
+			System.out.println("No se pudo cerrar");
+		}
+	}
+	
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		FachadaBD aplicacion = new FachadaBD();
+		aplicacion.conectar();
+	}
+
+}//Fin de la clase Fachada
