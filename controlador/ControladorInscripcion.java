a776d2aeb49ae05cf4ae185f1ee1c825cb83c2b0
DavidMG27 <33270740+DavidMG27@users.noreply.github.com>
2017-11-07 22:41:11 -0500

    Add files via upload

---
+++ controlador/ControladorInscripcion.java
@@ -0,0 +1,53 @@
+package controlador;
+
+import javax.swing.JOptionPane;
+
+import accesoDatos.DaoInscripcion;
+import accesoDatos.DaoPreInscripcion;
+import logica.Inscripcion;
+import logica.Usuario;
+import logica.preInscripcion;
+
+public class ControladorInscripcion {
+	
+	DaoInscripcion daoInscripcion;
+
+	public ControladorInscripcion() {
+		daoInscripcion = new DaoInscripcion();
+	}
+	
+	public preInscripcion consultarPreinscripcion(String cedula){
+		preInscripcion previa = new preInscripcion();
+		System.out.println("Se van a obtener los datos de la preinscripcion.");
+		
+		//Se llama al dao para consultar
+		previa = daoInscripcion.consultarPreinscripcion(cedula);
+		return previa;
+	}
+	
+	public boolean comprobarNoInscrito(String cedula){
+		String resultado = daoInscripcion.consultarNoinscrito(cedula);
+		if(resultado.equals("")){return false;}
+		else{return true;}
+	}
+	
+	public int insertarInscripcion (String nombre_evento, String nombre_participante, String cedula, String fecha, String telefono, String email){
+		Inscripcion ins = new Inscripcion(nombre_evento, nombre_participante, cedula, fecha, telefono, email);
+		
+		//Se llama al dao para guardar
+		System.out.println("Se va a insertar un Usuario");
+				
+		int result = daoInscripcion.guardarInscripcion(ins);
+		
+		if(result == 1){JOptionPane.showMessageDialog(null, "Se realizo la inscripcion correctamente.");}
+		else if(result == -1){JOptionPane.showMessageDialog(null, "No se pudo realizar la inscripcion.");}
+				
+		return result;
+	}
+	
+	
+	public void cerrarConexionBD(){
+        daoInscripcion.cerrarConexionBD();
+    }
+
+}
